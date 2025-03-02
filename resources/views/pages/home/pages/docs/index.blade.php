685ce5c1b6300e6ad33d08be0bc5f340ef157959
iammarjamal <ammar.jamal.jadallah@gmail.com>
2025-03-02 09:26:09 +0300

    fix and update

--- resources/views/pages/home/pages/docs/index.blade.php
+++ resources/views/pages/home/pages/docs/index.blade.php
@@ -6,36 +6,127 @@
 
         <div class="flex flex-col w-full min-h-screen mx-4 my-4 prose text-start text-dark dark:text-light dark:prose-invert"
             x-data="{ show: true }" x-init="document.addEventListener('livewire:navigate', () => { show = false });" :class="show ? 'xyz-in' : 'xyz-out'" xyz="fade">
-                @php
+            @php
                 $content = json_decode($document->body, true);
                 $htmlContent = '';
 
-                foreach ($content['blocks'] as $block) {
-                    if ($block['type'] == 'header') {
-                        $htmlContent .=
-                            '<h' .
-                            e($block['data']['level']) .
-                            '>' .
-                            e($block['data']['text']) .
-                            '</h' .
-                            e($block['data']['level']) .
-                            '>';
-                    } elseif ($block['type'] == 'paragraph') {
-                        // Add the email link to the paragraph block
-                        $text = e($block['data']['text']);
-                        $text = str_replace(
-                            'contribution@alpineuse.org',
-                            '<a href="mailto:contribution@alpineuse.org">contribution@alpineuse.org</a>',
-                            $text,
-                        );
-                        $htmlContent .= '<p>' . $text . '</p>';
-                    } elseif ($block['type'] == 'code') {
-                        $htmlContent .= '<pre><code>' . e($block['data']['code']) . '</code></pre>';
+                if (isset($content['blocks']) && is_array($content['blocks'])) {
+                    foreach ($content['blocks'] as $block) {
+                        switch ($block['type']) {
+                            case 'header':
+                                $level = $block['data']['level'] ?? 1;
+                                // For headers we assume plain text (you can adjust if needed)
+                                $text = e($block['data']['text'] ?? '');
+                                $htmlContent .= "<h{$level}>{$text}</h{$level}>";
+                                break;
+
+                            case 'paragraph':
+                                // Allow <a> tags so that links render as HTML.
+                                // Adjust allowed tags if needed.
+                                $allowedTags = '<a>';
+                                $text = strip_tags($block['data']['text'] ?? '', $allowedTags);
+                                $htmlContent .= "<p>{$text}</p>";
+                                break;
+
+                            case 'list':
+                                $tag = ($block['data']['style'] ?? 'unordered') === 'ordered' ? 'ol' : 'ul';
+                                $htmlContent .= "<{$tag}>";
+                                if (isset($block['data']['items']) && is_array($block['data']['items'])) {
+                                    foreach ($block['data']['items'] as $item) {
+                                        $htmlContent .= '<li>' . e($item) . '</li>';
+                                    }
+                                }
+                                $htmlContent .= "</{$tag}>";
+                                break;
+
+                            case 'image':
+                                $src = e($block['data']['file']['url'] ?? '');
+                                $caption = $block['data']['caption'] ?? '';
+                                $htmlContent .= '<figure>';
+                                $htmlContent .= '<img src="' . $src . '" alt="' . e($caption) . '">';
+                                if ($caption) {
+                                    $htmlContent .= '<figcaption>' . $caption . '</figcaption>';
+                                }
+                                $htmlContent .= '</figure>';
+                                break;
+
+                            case 'quote':
+                                $text = $block['data']['text'] ?? '';
+                                $caption = $block['data']['caption'] ?? '';
+                                $htmlContent .= '<blockquote>';
+                                $htmlContent .= $text;
+                                if ($caption) {
+                                    $htmlContent .= '<footer>' . $caption . '</footer>';
+                                }
+                                $htmlContent .= '</blockquote>';
+                                break;
+
+                            case 'code':
+                                $code = e($block['data']['code'] ?? '');
+                                $htmlContent .= '<pre><code>' . $code . '</code></pre>';
+                                break;
+
+                            case 'delimiter':
+                                $htmlContent .= '<hr>';
+                                break;
+
+                            case 'checklist':
+                                $htmlContent .= '<ul class="checklist">';
+                                if (isset($block['data']['items']) && is_array($block['data']['items'])) {
+                                    foreach ($block['data']['items'] as $item) {
+                                        $checked = !empty($item['checked']) ? 'checked' : '';
+                                        $htmlContent .= '<li>';
+                                        $htmlContent .= '<input type="checkbox" disabled ' . $checked . '>';
+                                        $htmlContent .= '<span>' . e($item['text'] ?? '') . '</span>';
+                                        $htmlContent .= '</li>';
+                                    }
+                                }
+                                $htmlContent .= '</ul>';
+                                break;
+
+                            case 'table':
+                                $htmlContent .= '<table>';
+                                if (!empty($block['data']['content']) && is_array($block['data']['content'])) {
+                                    foreach ($block['data']['content'] as $row) {
+                                        $htmlContent .= '<tr>';
+                                        foreach ($row as $cell) {
+                                            $htmlContent .= '<td>' . e($cell) . '</td>';
+                                        }
+                                        $htmlContent .= '</tr>';
+                                    }
+                                }
+                                $htmlContent .= '</table>';
+                                break;
+
+                            case 'embed':
+                                $data = $block['data'];
+                                $embedUrl = e($data['embed'] ?? '');
+                                $caption = $data['caption'] ?? '';
+                                $htmlContent .= '<div class="embed">';
+                                $htmlContent .=
+                                    '<a href="' . $embedUrl . '" target="_blank" rel="nofollow">' . $embedUrl . '</a>';
+                                if ($caption) {
+                                    $htmlContent .= '<div class="embed-caption">' . $caption . '</div>';
+                                }
+                                $htmlContent .= '</div>';
+                                break;
+
+                            case 'raw':
+                                // Output raw HTML (ensure this is safe)
+                                $htmlContent .= $block['data']['html'] ?? '';
+                                break;
+
+                            default:
+                                if (!empty($block['data']['text'])) {
+                                    $htmlContent .= '<p>' . $block['data']['text'] . '</p>';
+                                }
+                                break;
+                        }
                     }
                 }
-
-                print $htmlContent;
             @endphp
+            
+            {!! $htmlContent !!}
         </div>
     </div>
 </div>
